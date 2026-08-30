bb6bc82004caee945d20dade6dcb043b1c1289cc
Michel Ferreira <michelmf@poli.ufrj.br>
2026-08-30 02:14:54 -0300

    Add package info

--- src/ica/__init__.py
+++ src/ica/__init__.py
@@ -1,2 +1,12 @@
-def main() -> None:
-    print("Hello from ica!")
+"""
+ica is a package for loading, processing, and analyzing audio source data 
+for Independent Component Analysis (ICA) workflows.
+
+It provides dataset abstractions and tools to facilitate the handling of 
+audio files, ensuring consistency and ease of integration with machine learning 
+pipelines, especially those utilizing PyTorch.
+
+This package was created specifically for the ICA course. It is designed to 
+address the requirements of the class assignment, and is not intended as a 
+general-purpose package for broader applications.
+"""
